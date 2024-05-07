1c6dcf6d3a67b386439d91664be2e232bef9b568
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-05-07 15:54:36 -0500

    completed numbers and variables review

---
+++ scripts/numbers.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// Create a variable using the let keyword and assign a number to it.
+let firstNumber = 1
+console.log(firstNumber)
+
+// On a new line, add 15 to your variable
+firstNumber = firstNumber+15
+console.log(firstNumber)
+
+// On a new line, subtract 8 from your variable without using the variable when calculating the new value
+firstNumber -= 8
+console.log(firstNumber)
+
+// Calculate the average of 26, 44, 72, 86, 112. Assign the calculated average to a new variable.
+let calculateAverage = (26+44+72+86+112)/5
+
+let average = calculateAverage 
+console.log(average)
+
+// Modify the following code so it outputs 15 without adding any new lines of code.
+let theNumber = 14;
+console.log(++theNumber)
+
+// What operator can you use to find out if a variable contains a number?
+let randomNumber = 5
+console.log(typeof(randomNumber))
--- scripts/variables.js
+++ scripts/variables.js
@@ -1,3 +1,24 @@
 'use strict'
+//Declare and assign values to 3 variables using each of the following keywords let, const, and var.
+let quarter = .25
+const dollar = 1
+var fiftyDollar = 50
 
-console.log('hello')
+// Use console.log() to display the value of the variables in the browser's console.
+console.log(`quarter is :[${quarter}] dollar is: [${dollar}] fiftyDollar is: [${fiftyDollar}]`)
+
+//Change the value of the variables declared with the let and var. Print their new value to the console with console.log()
+quarter = .50
+fiftyDollar = 100
+console.log(`quarter is now: [${quarter}] fiftyDollar is now: [${fiftyDollar}]`)
+
+//What happens if you try to change the value of the variable declared with the const and print its new value to the console?
+// ^^^an error will happen because a const cannot be changed to a new value
+// ^^dollar = 2
+//^^ console.log(`${dollar}`)
+
+//Why might you choose to use the const keyword?
+// ^^^you want to use const when you don't want the value of a variable to change
+
+//What is one reason it not best practice to use the var keyword?
+// ^^^a reason for not using var keyword for best practice is becuase the var can be accessed anywhere in global scope so it can accidentally mess your code up
